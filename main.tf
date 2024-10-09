7fed852288e3b2e5f9e65a75e867d4727dec1cb8
O. Midiyanto <omidiyanto7@gmail.com>
2024-10-09 14:35:53 +0700

    update

--- main.tf
+++ main.tf
@@ -2,8 +2,20 @@ terraform {
   required_providers {
     proxmox = {
       source = "Telmate/proxmox"
-      version = "3.0.1-rc4"
+      version = "2.9.14"
     }
   }
 }
 
+resource "proxmox_vm_qemu" "vm" {
+  vmid = 200
+  name = "demo-vm"
+  target_node = "proxmox"
+  
+  clone = "ubuntu-template"
+  full_clone = true
+
+  os_type = "cloud-init"
+  cloudinit_cdrom_storage = "local"
+  
+}
