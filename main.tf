1ffe12202ae4179eed6adad6f8423afe4ec6085a
O. Midiyanto <omidiyanto7@gmail.com>
2024-10-09 14:41:17 +0700

    update

--- main.tf
+++ main.tf
@@ -17,5 +17,34 @@ resource "proxmox_vm_qemu" "vm" {
 
   os_type = "cloud-init"
   cloudinit_cdrom_storage = "local"
-  
+
+  ciuser = var.ci_user
+  cipassword = var.ci_password
+  sshkeys = file(var.ci_ssh_public_key)
+
+  cores = 1
+  memory = 1024
+  agent = 1
+
+  bootdisk = "scsi0"
+  scsihw = "virtio-scsi-pci"
+  ipconfig0 = "ip=dhcp"
+
+  disk {
+    size = "10G"
+    type = "scsi"
+    storage = "local"
+  }
+
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+    tag = "103"
+  }
+
+  lifecycle {
+    ignore_changes = [ 
+      network
+     ]
+  }
 }
