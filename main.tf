63315d4e4fe8e69b3d9966c977ae977796a22da8
O. Midiyanto <omidiyanto7@gmail.com>
2024-10-11 00:11:47 +0700

    update

--- main.tf
+++ main.tf
@@ -2,41 +2,64 @@ terraform {
   required_providers {
     proxmox = {
       source = "Telmate/proxmox"
-      version = "3.0.1-rc4"
+      version = "3.0.1-rc3"
     }
   }
 }
 
-resource "proxmox_vm_qemu" "vm" {
-  vmid = 200
-  name = "demo-vm"
-  target_node = "proxmox"
-  
-  clone = "ubuntu-template"
-  full_clone = true
+resource "proxmox_vm_qemu" "vm-demo" {
+    name = "vm-demo"
+    target_node = "proxmox"
 
-  os_type = "cloud-init"
-  
+    # The template name to clone this vm from
+    clone = "ubuntu-template"
+    full_clone = true
 
-  ciuser = var.ci_user
-  cipassword = var.ci_password
-  sshkeys = file(var.ci_ssh_public_key)
+    os_type = "cloud-init"
 
-  cores = 1
-  memory = 1024
-  agent = 1
+    ciuser = var.ci_user
+    cipassword = var.ci_password
+    sshkeys = file(var.ci_ssh_public_key)
 
-  bootdisk = "scsi0"
-  scsihw = "virtio-scsi-pci"
-  ipconfig0 = "ip=dhcp"
+    # Activate QEMU agent for this VM
+    agent = 1
+    cores = 1
+    memory = 1024
 
-  network {
-    model = "virtio"
-    bridge = "vmbr0"
-    tag = "103"
-  }
+    bootdisk = "scsi0"
+    scsihw = "virtio-scsi-pci"
 
-  lifecycle {
+    # Setup the disk
+    disks {
+        ide {
+            ide0 {
+                cloudinit {
+                    storage = "local"
+                }
+            }
+        }
+        scsi {
+            scsi0 {
+                disk {
+                    size    = 10
+                    storage = "local"
+                }
+            }
+        }
+    }
+
+    # Setup the network interface and assign a vlan tag: 256
+    network {
+        model = "virtio"
+        bridge = "vmbr0"
+    }
+
+    # Setup the ip address using cloud-init.
+    boot = "order=scsi0"
+    # Keep in mind to use the CIDR notation for the ip.
+    ipconfig0 = "ip=dhcp"
+    
+    lifecycle {
     ignore_changes = [ 
       network
      ]
